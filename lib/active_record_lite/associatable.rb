994a86f343fec035c9aca791dbd2aa513b845d87
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-12 07:32:03 -0700

    rewrite associatable.rb in OO way

--- lib/active_record_lite/associatable.rb
+++ lib/active_record_lite/associatable.rb
@@ -2,60 +2,78 @@ require 'active_support/core_ext/object/try'
 require 'active_support/inflector'
 require_relative './db_connection.rb'
 
+class AssocParams
+  attr_reader(
+    :foreign_key,
+    :other_class_name,
+    :primary_key,
+  )
+
+  def other_class
+    @other_class_name.constantize
+  end
+
+  def other_table
+    other_class.table_name
+  end
+end
+
+class BelongsToAssocParams < AssocParams
+  def initialize(name, params)
+    @foreign_key = params[:foreign_key] || "#{name}_id".to_sym
+    @other_class_name = params[:class_name] || name.to_s.camelcase
+    @primary_key = params[:primary_key] || :id
+  end
+
+  def type
+    :belongs_to
+  end
+end
+
+class HasManyAssocParams < AssocParams
+  def initialize(name, params)
+    @foreign_key = (params[:foreign_key] ||
+      "#{self.name.underscore}_id".to_sym)
+    @other_class_name = (params[:class_name] ||
+      name.to_s.singularize.camelcase)
+    @primary_key = params[:primary_key] || :id
+  end
+
+  def type
+    :has_many
+  end
+end
+
 module Associatable
   def belongs_to(name, params = {})
-    full_params = {
-      :other_class_name => params[:class_name] || name.to_s.camelcase,
-           :primary_key => params[:primary_key] || :id,
-           :foreign_key => params[:foreign_key] || "#{name}_id".to_sym,
-                  :type => :belongs_to
-    }
-
     @assoc_params ||= {}
-    @assoc_params[name] = full_params
+    aps = BelongsToAssocParams.new(name, params)
+    @assoc_params[name] = aps
 
     define_method(name) do
-      other_class = full_params[:other_class_name].constantize
-      other_table_name = other_class.table_name
-      primary_key = full_params[:primary_key]
-      foreign_key = full_params[:foreign_key]
-
-      results = DBConnection.execute(<<-SQL, self.send(foreign_key))
+      results = DBConnection.execute(<<-SQL, self.send(aps.foreign_key))
         SELECT *
-          FROM #{other_table_name}
-         WHERE #{other_table_name}.#{primary_key} = ?
+          FROM #{aps.other_table}
+         WHERE #{aps.other_table}.#{aps.primary_key} = ?
       SQL
 
-      other_class.parse_all(results).first
+      aps.other_class.parse_all(results).first
     end
   end
 
   def has_many(name, params = {})
-    full_params = {
-      :other_class_name => (params[:class_name] ||
-                              name.to_s.singularize.camelcase),
-           :primary_key => params[:primary_key] || :id,
-           :foreign_key => (params[:foreign_key] ||
-                              "#{self.name.underscore}_id".to_sym),
-                  :type => :has_many
-    }
-
     @assoc_params ||= {}
-    @assoc_params[name] = full_params
+    aps = HasManyAssocParams.new(name, params)
+    @assoc_params[name] = aps
 
     define_method(name) do
-      other_class = full_params[:other_class_name].constantize
-      other_table_name = other_class.table_name
-      primary_key = full_params[:primary_key]
-      foreign_key = full_params[:foreign_key]
-
-      results = DBConnection.execute(<<-SQL, self.send(primary_key))
+      results = DBConnection.execute(<<-SQL, self.send(aps.primary_key))
         SELECT *
-          FROM #{other_table_name}
-         WHERE #{other_table_name}.#{foreign_key} = ?
+          FROM #{aps.other_table}
+         WHERE #{aps.other_table}.#{aps.foreign_key} = ?
       SQL
 
-      other_class.parse_all(results)
+      aps.other_class.parse_all(results)
     end
   end
 end
