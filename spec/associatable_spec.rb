0887804f2a537f8f6dd30ac2debd920da89873e5
CJ Avilla <cjavilla@gmail.com>
2013-10-03 13:04:08 -0700

    updating searchable

--- spec/associatable_spec.rb
+++ spec/associatable_spec.rb
@@ -1,37 +1,29 @@
 require 'active_record_lite'
+describe "searchable" do
+  before(:all) do
+    cats_db_file_name =File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
+    DBConnection.open(cats_db_file_name)
 
-# https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
-cats_db_file_name =
-  File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
-DBConnection.open(cats_db_file_name)
+    class Cat < SQLObject
+      set_table_name("cats")
+      my_attr_accessible(:id, :name, :owner_id)
+    end
 
-class Cat < SQLObject
-  set_table_name("cats")
-  my_attr_accessible(:id, :name, :owner_id)
+    class Human < SQLObject
+      set_table_name("humans")
+      my_attr_accessible(:id, :fname, :lname, :house_id)
+    end
+  end
 
-  belongs_to :human, :class_name => "Human", :primary_key => :id, :foreign_key => :owner_id
-  has_one_through :house, :human, :house
+  describe "#where" do
+    it "returns correct cat" do
+      cat = Cat.where(:name => "Breakfast")[0]
+      cat.name.should == "Breakfast"
+    end
+
+    it "returns correct human" do
+      human = Human.where(:fname => "Matt", :house_id => 1)[0]
+      human.fname.should == "Matt"
+    end
+  end
 end
-
-class Human < SQLObject
-  set_table_name("humans")
-  my_attr_accessible(:id, :fname, :lname, :house_id)
-
-  has_many :cats, :foreign_key => :owner_id
-  belongs_to :house
-end
-
-class House < SQLObject
-  set_table_name("houses")
-  my_attr_accessible(:id, :address, :house_id)
-end
-
-cat = Cat.find(1)
-p cat
-p cat.human
-
-human = Human.find(1)
-p human.cats
-p human.house
-
-p cat.house
