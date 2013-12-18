d3182e863b363122769aacf6674348f361806d4c
Ned Ruggeri <ruggeri@self-loop.com>
2013-12-18 11:19:35 -0800

    add describe blocks to 02_sql_object_spec to fix ordering

--- spec/02_sql_object_spec.rb
+++ spec/02_sql_object_spec.rb
@@ -19,46 +19,50 @@ describe SQLObject do
     end
   end
 
-  it "::set_table_name sets table name" do
-    expect(Human.table_name).to eq("humans")
-  end
+  describe "::set_table/::table_name" do
+    it "::set_table_name sets table name" do
+      expect(Human.table_name).to eq("humans")
+    end
 
-  it "::table_name generates default name" do
-    expect(Cat.table_name).to eq("cats")
-  end
+    it "::table_name generates default name" do
+      expect(Cat.table_name).to eq("cats")
+    end
 
-  it "::parse_all turns an array of hashes into objects" do
-    hashes = [
-      { :name => "cat1", :owner_id => 1 },
-      { :name => "cat2", :owner_id => 2 }
-    ]
+    it "::parse_all turns an array of hashes into objects" do
+      hashes = [
+        { :name => "cat1", :owner_id => 1 },
+        { :name => "cat2", :owner_id => 2 }
+      ]
 
-    cats = Cat.parse_all(hashes)
-    expect(cats.length).to eq(2)
-    hashes.each_index do |i|
-      expect(cats[i].name).to eq(hashes[i][:name])
-      expect(cats[i].owner_id).to eq(hashes[i][:owner_id])
+      cats = Cat.parse_all(hashes)
+      expect(cats.length).to eq(2)
+      hashes.each_index do |i|
+        expect(cats[i].name).to eq(hashes[i][:name])
+        expect(cats[i].owner_id).to eq(hashes[i][:owner_id])
+      end
     end
   end
 
-  it "::all returns all the cats" do
-    cats = Cat.all
+  describe "::all/::find" do
+    it "::all returns all the cats" do
+      cats = Cat.all
 
-    expect(cats.count).to eq(4)
-    cats.all? { |cat| expect(cat).to be_instance_of(Cat) }
-  end
+      expect(cats.count).to eq(4)
+      cats.all? { |cat| expect(cat).to be_instance_of(Cat) }
+    end
 
-  it "#attribute_values returns array of values" do
-    cat = Cat.new(:id => 123, :name => "cat1", :owner_id => 1)
+    it "#attribute_values returns array of values" do
+      cat = Cat.new(:id => 123, :name => "cat1", :owner_id => 1)
 
-    expect(cat.attribute_values).to eq([123, "cat1", 1])
-  end
+      expect(cat.attribute_values).to eq([123, "cat1", 1])
+    end
 
-  it "::find finds objects by id" do
-    c = Cat.find(1)
+    it "::find finds objects by id" do
+      c = Cat.find(1)
 
-    expect(c).not_to be_nil
-    expect(c.name).to eq("Breakfast")
+      expect(c).not_to be_nil
+      expect(c.name).to eq("Breakfast")
+    end
   end
 
   describe "#insert" do
@@ -83,26 +87,30 @@ describe SQLObject do
     end
   end
 
-  it "#update changes attributes" do
-    human = Human.find(2)
+  describe "#update" do
+    it "#update changes attributes" do
+      human = Human.find(2)
 
     human.fname = "Matthew"
     human.lname = "von Rubens"
     human.update
 
-    # pull the human again
-    human = Human.find(2)
-    expect(human.fname).to eq("Matthew")
-    expect(human.lname).to eq("von Rubens")
+      # pull the human again
+      human = Human.find(2)
+      expect(human.fname).to eq("Matthew")
+      expect(human.lname).to eq("von Rubens")
+    end
   end
 
-  it "#save calls save/update as appropriate" do
-    human = Human.new
-    expect(human).to receive(:insert)
-    human.save
+  describe "#save" do
+    it "#save calls save/update as appropriate" do
+      human = Human.new
+      expect(human).to receive(:insert)
+      human.save
 
-    human = Human.find(1)
-    expect(human).to receive(:update)
-    human.save
+      human = Human.find(1)
+      expect(human).to receive(:update)
+      human.save
+    end
   end
 end
